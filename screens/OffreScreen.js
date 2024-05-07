a9b29e2801c9379ce66b81dc983924f60a0a1e1f
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-05-07 11:43:13 +0200

    Un peu de CSS sur les offres

--- screens/OffreScreen.js
+++ screens/OffreScreen.js
@@ -8,9 +8,26 @@ const OffreDetailsScreen = ({ route }) => {
     return (
         <View style={styles.container}>
             <Text style={styles.offreTitre}>{offre.titre}</Text>
-            <Text>{offre.entreprise}</Text>
-            <Text>{offre.lieu}</Text>
-            <Text>{offre.date_publication}</Text>
+            <View style={styles.infoContainer}>
+                <Text style={styles.label}>Entreprise:</Text>
+                <Text style={styles.detail}>{offre.entreprise}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.label}>Lieu:</Text>
+                <Text style={styles.detail}>{offre.lieu}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.label}>Date de publication:</Text>
+                <Text style={styles.detail}>{offre.date_publication}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.label}>Description:</Text>
+                <Text style={styles.detail}>{offre.description}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.label}>Compétences requises:</Text>
+                <Text style={styles.detail}>{offre.competences}</Text>
+            </View>
             {/* Ajoutez d'autres détails de l'offre ici si nécessaire */}
         </View>
     );
@@ -24,8 +41,20 @@ const styles = StyleSheet.create({
     },
     offreTitre: {
         fontWeight: 'bold',
-        fontSize: 20,
-        marginBottom: 10,
+        fontSize: 24,
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    infoContainer: {
+        marginBottom: 15,
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginBottom: 5,
+    },
+    detail: {
+        fontSize: 18,
     },
 });
 
